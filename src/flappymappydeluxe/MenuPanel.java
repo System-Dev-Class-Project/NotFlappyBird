a5152adea86145d33d30f9ed28ffd5fa91be1ae9
JoeKion <joekion57@gmail.com>
2024-06-30 18:50:58 +0200

    Final small fixes

--- src/flappymappydeluxe/MenuPanel.java
+++ src/flappymappydeluxe/MenuPanel.java
@@ -16,24 +16,23 @@ public class MenuPanel extends JPanel {
     private Button startButton;  // Initializing a button to start the game with
     private Button shopButton;  // Initializing a button to enter the shop with
     private Button settingsButton;  // Initializing a button to enter the settings with
-    private BufferedImage img;  
-    private BufferedImage img1;  
-    private BufferedImage img2;
-    public boolean StartingPoint = false;
+    private BufferedImage img;  //the menu background image
+    private BufferedImage img1;  //main menu game title image
+    public boolean StartingPoint = false;  //sets the game time to false every time the MenuPanel is opened so the game doesn't keep going in the background
     private int imgX = 0; // Initial x-coordinate of the image
     public static AudioPlayer audioPlayer; // Add an AudioPlayer instance
-    private Timer visibilityCheckTimer;
-
-    private CardLayout cardLayout;
+    
+    private CardLayout cardLayout;    //MenuPanel added to the cardLayout mainPanel
     private JPanel mainPanel;
     
     public MenuPanel(CardLayout cardLayout, JPanel mainPanel, AudioPlayer audioPlayer) {
-        this.audioPlayer = audioPlayer;
+        this.audioPlayer = audioPlayer;  //local audioPlayer instance to handle background themes
         this.cardLayout = cardLayout;
         this.mainPanel = mainPanel;
         LoadImage();
         audioPlayer.play("NotFlappyBird-main/Music/1-01. Main Theme (Title Screen).wav"); // Plays the background music
         
+        // Moves the background image in an appropriate timeframe
         Timer timer = new Timer(10, new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 // Update background position
@@ -59,12 +58,12 @@ public class MenuPanel extends JPanel {
                 super.mouseClicked(e);
                 if (startButton.isClicked(e)) {
                     FlappyClass.restartGame();  // Restart the game
-                    StartingPoint = true;
-                    cardLayout.show(mainPanel, "game");
-                    switchMusic("NotFlappyBird-main/Music/1-04.-Strike-the-Earth_-_Plains-of-Passage_.wav");
-                    System.out.println("Play Button clicked");
+                    StartingPoint = true; //we set startingPoint to true so that the game time resumes
+                    cardLayout.show(mainPanel, "game"); //swap to game panel
+                    switchMusic("NotFlappyBird-main/Music/1-04.-Strike-the-Earth_-_Plains-of-Passage_.wav"); //music swap to game music theme
+                    System.out.println("Play Button clicked"); //print response to confirm
                 }
-               
+               //same procedure for the other action listeners minus the game time start
                 if (shopButton.isClicked(e)) {
                     cardLayout.show(mainPanel, "shop");
                     switchMusic("NotFlappyBird-main/Music/Ace-Attorney-18-Marvin-Grossberg-_-Age_-Regret_-Retribution.wav");
@@ -90,35 +89,26 @@ public class MenuPanel extends JPanel {
     // Loads the menu panel image 
     private void LoadImage() {
         try {
-            img = ImageIO.read(new File("NotFlappyBird-main/Images/pixelartbackground.png"));
-            img1 = ImageIO.read(new File("NotFlappyBird-main/Images/CoinBig.png"));
-            img2 = ImageIO.read(new File("NotFlappyBird-main/Images/NotFlappyBirdTitle.png"));
+            img = ImageIO.read(new File("NotFlappyBird-main/Images/pixelartbackground.png")); //main menu background
+            img1 = ImageIO.read(new File("NotFlappyBird-main/Images/NotFlappyBirdTitle.png")); //title image NotFlappyBird
         } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
 
-    // Visualizes the loaded image in the entire menu panel. We use paintComponent to visualize the main menu image as well as buttons.
+    // Visualizes the loaded images in the entire menu panel. We use paintComponent to visualize the main menu image as well as buttons and the title.
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.drawImage(img, imgX, 0, null);
         g.drawImage(img, imgX + img.getWidth(), 0, null);
-        if (imgX + img.getWidth() < getWidth()) {
+        if (imgX + img.getWidth() < getWidth()) {                       //The background image is repainted if it has moved its entire length
             g.drawImage(img, imgX + img.getWidth() * 2, 0, null);
         }
-        g.drawImage(img2, 30, 25, null);
+        g.drawImage(img1, 30, 25, null);
         startButton.draw(g);
         shopButton.draw(g);
         settingsButton.draw(g);
     }
-
-    // This concerns the menu panel background. We decided that it would look more lively if the background moved here as well.
-    // For simplicity, we move the menu background with the same speed as the walls.
-    public void MenuMove() {
-        imgX += WallImage.speed;
-        if (imgX == -2048) {
-            imgX = 0;
-        }
-    }
+    
 }
